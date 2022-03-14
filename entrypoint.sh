d5b4ffc99cbcef362c89d0e61f3c8bbdd7683c1c
Chenyt26 <2396164919@qq.com>
2022-03-14 16:47:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(<kubectl.sha256) kubectl" | sha256sum --check
 
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-kubectl version --client
+chmod +x kubectl
+mkdir -p ~/.local/bin/kubectl
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version
