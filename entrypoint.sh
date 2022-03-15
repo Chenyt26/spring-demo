93e3f0df7c59ab67dfa158944cc0f5a368b5a7e3
Chenyt26 <2396164919@qq.com>
2022-03-15 10:22:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,4 +19,4 @@ kubectl get deploy -n cci-namespace-70395701
 kubectl get pods -n cci-namespace-70395701
 kubectl get rs -n cci-namespace-70395701
 kubectl describe deploy cci-deployment-20223141 -n cci-namespace-70395701
-# kubectl set image deployment/cci-deployment-20223141 container-0=swr.cn-north-4.myhuaweicloud.com/hcloudcli/demo:v1.1 
+kubectl set image deploy cci-deployment-20223141 container-0=swr.cn-north-4.myhuaweicloud.com/hcloudcli/demo:v1.1 -n cci-namespace-70395701
